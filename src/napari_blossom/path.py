9bd640d3030306885e682fd8488534238aa33778
Herearii M <93375163+hereariim@users.noreply.github.com>
2022-08-08 16:32:45 +0200

    Add files via upload

---
+++ src/napari_blossom/path.py
@@ -0,0 +1,14 @@
+import os.path
+# from blossom import config
+
+# CONF = config.get_conf_dict()
+homedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# base_dir = CONF['general']['base_directory']
+base_dir = "."
+
+def get_base_dir():
+    return os.path.abspath(os.path.join(homedir, base_dir))
+
+def get_models_dir():
+     return os.path.join(get_base_dir(), "conidie")
