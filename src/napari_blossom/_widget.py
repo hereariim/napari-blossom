c10ec7a5bf8089cce242b6f23172b4500b73e9c5
Herearii M <herearii.metuarea@gmail.com>
2022-09-01 18:55:54 +0200

    ze parti

--- src/napari_blossom/_widget.py
+++ src/napari_blossom/_widget.py
@@ -13,32 +13,73 @@ import pathlib
 import napari_blossom.path as paths
 from skimage.io import imread
 
+def get_mosaic(img):
+  A = []
+  h,l,z = img.shape
+  #longueur
+  L1 = [ i for i in range(0,l-255,255)]+[l-255]
+  L2 = [ 256+i for i in range(0,l,255) if 256+i < l]+[l]
+
+  #hauteur
+  R1 = [ i for i in range(0,h-255,255)]+[h-255]
+  R2 = [ 256+i for i in range(0,h,255) if 256+i < h]+[h]
+
+  for h1,h2 in zip(R1,R2):
+    for l1,l2 in zip(L1,L2):
+      A.append(img[h1:h2,l1:l2])
+  return A
+
+def reconstruire(img1,K):
+  ex,ey,ez=img1.shape
+  A = np.zeros((ex,ey,1), dtype=np.bool)
+  h=ex
+  l=ey
+  z=1
+  
+  #longueur
+  L1 = [ i for i in range(0,l-255,255)]+[l-255]
+  L2 = [ 256+i for i in range(0,l,255) if 256+i < l]+[l]
+
+  #hauteur
+  R1 = [ i for i in range(0,h-255,255)]+[h-255]
+  R2 = [ 256+i for i in range(0,h,255) if 256+i < h]+[h]
+  
+  n = 0
+  for h1,h2 in zip(R1,R2):
+    for l1,l2 in zip(L1,L2):
+      if A[h1:h2,l1:l2].shape == K[n].shape:
+        A[h1:h2,l1:l2] = K[n]
+      else:
+        A[h1:h2,l1:l2] = np.zeros(A[h1:h2,l1:l2].shape, dtype=np.bool)
+      n+=1
+  return A
+
+def dice_coefficient(y_true, y_pred):
+    eps = 1e-6
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection) / (K.sum(y_true_f * y_true_f) + K.sum(y_pred_f * y_pred_f) + eps) #eps pour éviter la division par 0 
+
 def do_image_segmentation(
     layer: ImageData
     ) -> ImageData:
     
-    def redimension(image):
-        X = np.zeros((1,256,256,3),dtype=np.uint8)   
-        size_ = image.shape
-        img = image[:,:,:3]
-        X[0] = resize(img, (256, 256), mode='constant', preserve_range=True)
-        return X,size_
+    img1_list = get_mosaic(layer)
 
-    def dice_coefficient(y_true, y_pred):
-        eps = 1e-6
-        y_true_f = K.flatten(y_true)
-        y_pred_f = K.flatten(y_pred)
-        intersection = K.sum(y_true_f * y_pred_f)
-        return (2. * intersection) / (K.sum(y_true_f * y_true_f) + K.sum(y_pred_f * y_pred_f) + eps) #eps pour éviter la division par 0 
-    
-    image_reshaped,size_ = redimension(layer)
-    v = os.path.abspath(__file__)
-    model_new = tf.keras.models.load_model(os.path.join(paths.get_models_dir(),"best_model_FL_BCE_0_5.h5"),custom_objects={'dice_coefficient': dice_coefficient})
-    prediction = model_new.predict(image_reshaped)
-    preds_test_t = (prediction > 0.2).astype(np.uint8)
-    temp=np.squeeze(preds_test_t[0,:,:,0])*255
-    
-    return cv2.resize(temp, dsize=(size_[1],size_[0]))
+    model_New = tf.keras.models.load_model(os.path.join(paths.get_models_dir(),'best_model_W_BCE_chpping.h5'),custom_objects={'dice_coefficient': dice_coefficient})
+
+    taille_p = 256
+    X_ensemble = np.zeros((len(img1_list), taille_p, taille_p, 3), dtype=np.uint8)
+    for n in range(len(img1_list)):
+      sz1_x,sz2_x,sz3_x = img1_list[n].shape
+      if (sz1_x,sz2_x)==(256,256):
+        X_ensemble[n]=img1_list[n]
+
+    preds_test = model_New.predict(X_ensemble, verbose=1)
+    preds_test_opt = (preds_test > 0.2).astype(np.uint8)
+    output_image = reconstruire(layer,preds_test_opt)
+    return np.squeeze(output_image[:,:,0])
 
 @magic_factory(call_button="Load",filename={"label": "Pick a file:"})
 def get_data(filename=pathlib.Path.cwd()) -> ImageData:
@@ -46,6 +87,6 @@ def get_data(filename=pathlib.Path.cwd()) -> ImageData:
 
 @magic_factory(call_button="Run")
 def do_model_segmentation(
-    layer: ImageData) -> ImageData:
+    layer: ImageData) -> LabelsData:
     show_info('Succes !')
     return do_image_segmentation(layer)
