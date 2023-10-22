aa8c5d75257bafb2877f9879e87e6a78a0b84f9c
hereariim <herearii.metuarea@gmail.com>
2023-10-22 21:16:07 +0200

    version 0.1.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="herearii.metuarea@gmail.com",
     name='napari-blossom', 
     description='Segmentation of blossom apple tree images',  
-    version="0.1.3",    
+    version="0.1.4",    
     long_description=long_description, 
     long_description_content_type="text/markdown",  
     url='https://github.com/hereariim/napari-blossom', 
